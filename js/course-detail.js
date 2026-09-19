dd72e14c534e05efe9cb3704256ae2a97cfd06bb
Fares-alnajjar <your_email@example.com>
2026-09-19 04:14:24 +0300

    adding base for course details

---
+++ js/course-detail.js
@@ -0,0 +1,34 @@
+const b1 = document.getElementById("c");
+b1.onclick = () => {
+    window.location.href = "https://www.geeksforgeeks.org/c-sharp/csharp-programming-language/";
+}
+
+const b2 = document.getElementById("oop");
+b2.onclick = () => {
+    window.location.href = "https://www.geeksforgeeks.org/dsa/introduction-of-object-oriented-programming/";
+}
+
+const b3 = document.getElementById("sql");
+b3.onclick = () => {
+    window.location.href = "https://www.w3schools.com/sql/sql_server.asp";
+}
+
+const b4 = document.getElementById("net");
+b4.onclick = () => {
+    window.location.href = "https://dotnet.microsoft.com/en-us/";
+}
+
+const b5 = document.getElementById("ent");
+b5.onclick = () => {
+    window.location.href = "https://www.geeksforgeeks.org/c-sharp/what-is-entity-framework-in-net-framework/";
+}
+
+const b6 = document.getElementById("rest");
+b6.onclick = () => {
+    window.location.href = "https://www.geeksforgeeks.org/node-js/rest-api-introduction/";
+}
+
+const b7 = document.getElementById("youtube");
+b7.onclick = () => {
+    window.location.href = "https://www.youtube.com/watch?v=MFsYaRnrcPQ";
+}
